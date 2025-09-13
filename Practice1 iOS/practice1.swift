02b7eb117f330551b48724a1adee40db194770e8
Suleimenova Zhasmin <zhasmin@MacBook-Pro-Suleimenova.local>
2025-09-13 18:21:46 +0500

    firs commit

---
+++ Practice1 iOS/practice1.swift
@@ -0,0 +1,37 @@
+//
+//  practice1.swift
+//  
+//
+//  Created by Suleimenova Zhasmin on 13.09.2025.
+//
+
+import Foundation
+let firstName: String = "Zhasmin"
+let lastName: String = "Suleimenova"
+let currentYear: Int = 2025
+let birthYear: Int = 2004
+let age: Int = currentYear - birthYear
+let isStudent: Bool = true
+let statusOfStudent: String = isStudent ? "student 👩🏻‍🎓" : "not a student"
+let height: Double = 1.55
+let weight: Double = 52
+let numberOfSiblings: Int = 1
+let hobby: String = "Reading 📖"
+let favoriteBook: String = "Marin Eden"
+let nuberOfHObbies: Int = 5
+let favoriteNumber: Int = 14
+let favoriteColor: String = "green 💚 "
+let isHobbyCreative: Bool = true
+let statusOfCreative: String = isHobbyCreative ? "creative 👩🏻‍🎨 " : " not creative"
+let futureGoals: String = "become a  sucssesful software developer 👩‍💻 "
+let lifeStory: String = "My name is \(firstName) \(lastName). I am \(age) years old, and I was born in \(birthYear). I am currently \(statusOfStudent). I am \(height) meters tall and I weigh \(weight) kilograms. I have \(numberOfSiblings) siblings. My favorite hobby is \(hobby), and my favorite book is \(favoriteBook). I think my hobby is \(statusOfCreative). I have \(nuberOfHObbies) hobbies in total, and my favorite number is \(favoriteNumber). My favorite color is \(favoriteColor). I hope to \(futureGoals)."
+
+print(lifeStory)
+
+
+
+
+
+
+
+
